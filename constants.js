8db94c1c27116586a8d46a86e4da7b6ca3594d6f
superdevpp <superdevpp@gmail.com>
2025-05-30 05:10:37 -0400

    [*] bug

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 export const GRAPHQL_BASE_ENDPOINT = 'https://thegraph.com/explorer/api/playground/';
 
-export const CSV_HEADER = 'dex_name,pool_id,pair,token0_id,token0_decimals,token0_symbol,token0_name,token1_id,token1_decimals,token1_symbol,token1_name';
+export const CSV_HEADER = 'dex_name,protocol,pool_id,pair,token0_id,token0_decimals,token0_symbol,token0_name,token1_id,token1_decimals,token1_symbol,token1_name';
 
 // Uniswap V2 Style
 export const POOLS_UNISWAP_V2 = 200;//1000;
